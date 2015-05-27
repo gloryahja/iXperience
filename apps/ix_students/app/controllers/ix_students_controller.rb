2aa62d04eca8ad43618ef8c9c70551ed9ba7ca97
Gloryah Allen <gja2nd@virginia.edu>
2015-05-27 18:18:41 +0200

    Add names

--- apps/ix_students/app/controllers/ix_students_controller.rb
+++ apps/ix_students/app/controllers/ix_students_controller.rb
@@ -4,5 +4,7 @@ class IxStudentsController < ApplicationController
   end
 
   def names
+  	@ix_names = ["Gloryah Allen", "Emily Dukas", "Molly Davey", "Zack Hyman", "Kaan Pinar", "Kate Ernst", "Pavi Vetri", "Oscar Wang", "Austin Rief", "Sophie Tao", "Matthew Biggins", "Daniel Low", "Yunny Chung", "Enrique Pajuelo", "Alex Schwartz", "Asad Khan", "Charlotte Searle", "Blake Altman", "Jack O'Rourke", "Susana Yepes", "Arya Naik", "Olivia Bicks", "Stanley Zuo", "Dilys Lambert", "Bailey Berro", "Han Ding", "Urvish Patel", "Ben Penchas"]
   end
+  
 end
